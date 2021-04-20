753d2d8960a24446db4e25dade969decfdfb8b58
vonrimS <mr.mytab@gmail.com>
2021-04-20 13:47:41 +0300

    first modified file commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,6 @@
 const request = require('request')
 //
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+
 
 
 const forecast = (longitude, latitude, callback) => {
